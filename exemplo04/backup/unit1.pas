612d77396d98160b17692447c305f50478b27664
Yago Renan <byago7408@gmail.com>
2022-06-27 20:25:18 -0500

    versao 0.0.3

---
+++ exemplo04/backup/unit1.pas
@@ -0,0 +1,56 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Buttons;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    BitBtn1: TBitBtn;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    procedure BitBtn1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.BitBtn1Click(Sender: TObject);
+var
+  nasc, atual, idade: integer;
+begin
+              nasc  := StrToInt(edNasc.text);
+              atual := StrToInt(edAtual.text);
+              idade := atual - nasc;
+        edIdade.text :=IntToStr(Idade);
+
+end;
+
+end.
+
